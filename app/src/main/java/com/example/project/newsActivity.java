184d6e2f3387846fbf029c9adf0e60ca12daa467
saranazly2000 <basheernazly@hotmail.com>
2021-07-04 15:39:29 +0300

    second commit

--- app/src/main/java/com/example/project/newsActivity.java
+++ app/src/main/java/com/example/project/newsActivity.java
@@ -32,14 +32,11 @@ public class newsActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private List<News> newsList;
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
         queue = Volley.newRequestQueue(getApplicationContext());
-
-
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -57,45 +54,22 @@ public class newsActivity extends AppCompatActivity {
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-
-
-        final List<News> ss = new ArrayList<News>();
         JsonObjectRequest jsonObject = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-
                 try {
-                    JSONArray jsonArray = response.getJSONArray("articles");
+                    JSONArray jsonArray = response.getJSONArray("latest");
                     for (int i = 0; i < jsonArray.length(); i++) {
                         JSONObject news = jsonArray.getJSONObject(i);
-                      //  statics statics = new statics();
-
                         String title = news.getString("title");
                         String description = news.getString("description");
                         String publishedAt = news.getString("publishedAt");
-
                         News news1 = new News(title,description,publishedAt);
                         newsList.add(news1);
                         adapter = new newsAdapter(newsList,this);
                         recyclerView.setAdapter(adapter);
-                     //   statics.setCountry(Country);
-                        /*String TotalConfirmed = country.getString("TotalConfirmed");
-                        statics.setTotalConfirmed(TotalConfirmed);
-                        String TotalDeaths = country.getString("TotalDeaths");
-                        statics.setTotalDeaths(TotalDeaths);
-                        String TotalRecovered = country.getString("TotalRecovered");
-                        statics.setTotalRecovered(TotalRecovered);*/
-
-                      //  ss.add(statics);
-
-
-
                         Log.e("title",title);
                         Log.e("description",description);
-
-                      //  Log.e("sara",ss.toString());
-
-
 
                     }
                 } catch (JSONException e) {
@@ -112,7 +86,6 @@ public class newsActivity extends AppCompatActivity {
         @Override
         public Map<String, String> getHeaders() throws AuthFailureError {
             HashMap<String, String> headers = new HashMap<String, String>();
-            //headers.put("Content-Type", "application/json");
             headers.put("User-Agent", "user-agent=Mozilla/5.0");
             return headers;
         }
